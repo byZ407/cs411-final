ab1bf60f88b38a192c85a90f16aa468b0270bf06
byZ407 <byz040407@outlook.com>
2025-04-29 00:50:37 -0400

    updated venv setup script

--- weatherapp/setup_venv.sh
+++ weatherapp/setup_venv.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Set the name of the virtual environment directory
-VENV_DIR="meal_max_venv"
-REQUIREMENTS_FILE="requirements.lock"
+VENV_DIR="venv"
+REQUIREMENTS_FILE="requirements.txt"
 
 # Check if the virtual environment already exists
 if [ ! -d "$VENV_DIR" ]; then
   echo "Creating virtual environment..."
-  python -m venv "$VENV_DIR"
+  python3 -m venv "$VENV_DIR"
 
   source "$VENV_DIR/bin/activate"
 
-  # Install dependencies from requirements.lock if it exists
+  # Install dependencies from requirements.txt if it exists
   if [ -f "$REQUIREMENTS_FILE" ]; then
     echo "Installing dependencies from $REQUIREMENTS_FILE..."
     pip install --no-cache-dir -r "$REQUIREMENTS_FILE"
@@ -23,3 +23,4 @@ else
   source "$VENV_DIR/bin/activate"
   echo "Virtual environment already exists. Activated."
 fi
+
